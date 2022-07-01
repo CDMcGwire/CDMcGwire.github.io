f40a3214e4add800b55af8920c0a282955c341ea
Connor McGwire <cdmcgwire@live.com>
2022-07-01 12:18:36 -0700

    Updating site

---
+++ _app/immutable/pages/novels/index.svelte-53070555.js
@@ -0,0 +1,21 @@
+import{S as H,i as K,s as O,e as m,k as p,t as b,c as f,m as v,a as g,h as _,d as n,R as q,b as t,g as k,J as s,n as B}from"../../chunks/index-e38d0f9f.js";var S="/_app/immutable/assets/metanoia-39fc5d9f.png",U="/_app/immutable/assets/tower-2899c88d.png";function C(G){let i,F,w,o,x,y,a,l,I,J,d,E,M,u,A,N,c,z;return{c(){i=m("img"),w=p(),o=m("a"),x=b("Available now on Kindle \u276F"),y=p(),a=m("div"),l=m("img"),J=p(),d=m("p"),E=b(`\u201CJoule: a young city founded by a faction of America\u2019s elite claiming it would become \u201Cthe spark\r
+		of a new era of Humanity.\u201D Most of the world saw it as a dangerous playhouse for a power-mad\r
+		oligarchy, but to people like John Nakae it was a chance to escape a life of stagnation and\r
+		decay. For five years, it was exactly that... Yet, real change proved difficult to grasp. Even\r
+		as the people around him pursued progress with more fervency than any generation before, nothing\r
+		had fundamentally changed for him or humanity.`),M=p(),u=m("p"),A=b(`\u201CUntil one night, under the shadows of that city\u2019s shining towers, the new era truly began as a\r
+		nightmare became flesh.`),N=p(),c=m("p"),z=b(`\u201CJohn nearly meets his end at the clawed hands of that nightmare, but in the aftermath discovers\r
+		a similar phenomenon has blessed him with the potential to twist a part of reality to his own\r
+		will. Others like him emerge in the city \u2014 as do ever more abominations \u2014 and he soon finds\r
+		himself on the frontline of an investigation into the growing mystery. Not merely to understand,\r
+		but to control.\u201D`),this.h()},l(e){i=f(e,"IMG",{src:!0,alt:!0,class:!0}),w=v(e),o=f(e,"A",{class:!0,target:!0,href:!0});var r=g(o);x=_(r,"Available now on Kindle \u276F"),r.forEach(n),y=v(e),a=f(e,"DIV",{class:!0});var h=g(a);l=f(h,"IMG",{src:!0,alt:!0,id:!0,class:!0}),J=v(h),d=f(h,"P",{class:!0});var P=g(d);E=_(P,`\u201CJoule: a young city founded by a faction of America\u2019s elite claiming it would become \u201Cthe spark\r
+		of a new era of Humanity.\u201D Most of the world saw it as a dangerous playhouse for a power-mad\r
+		oligarchy, but to people like John Nakae it was a chance to escape a life of stagnation and\r
+		decay. For five years, it was exactly that... Yet, real change proved difficult to grasp. Even\r
+		as the people around him pursued progress with more fervency than any generation before, nothing\r
+		had fundamentally changed for him or humanity.`),P.forEach(n),M=v(h),u=f(h,"P",{class:!0});var T=g(u);A=_(T,`\u201CUntil one night, under the shadows of that city\u2019s shining towers, the new era truly began as a\r
+		nightmare became flesh.`),T.forEach(n),N=v(h),c=f(h,"P",{class:!0});var Y=g(c);z=_(Y,`\u201CJohn nearly meets his end at the clawed hands of that nightmare, but in the aftermath discovers\r
+		a similar phenomenon has blessed him with the potential to twist a part of reality to his own\r
+		will. Others like him emerge in the city \u2014 as do ever more abominations \u2014 and he soon finds\r
+		himself on the frontline of an investigation into the growing mystery. Not merely to understand,\r
+		but to control.\u201D`),Y.forEach(n),h.forEach(n),this.h()},h(){q(i.src,F=S)||t(i,"src",F),t(i,"alt","Metanoia Title"),t(i,"class","svelte-m1khuf"),t(o,"class","kindle-link svelte-m1khuf"),t(o,"target","_blank"),t(o,"href","https://www.amazon.com/gp/product/B0B4FJY836/ref=dbs_a_def_rwt_bibl_vppi_i0"),q(l.src,I=U)||t(l,"src",I),t(l,"alt","Metanoia Title"),t(l,"id","tower-flow"),t(l,"class","svelte-m1khuf"),t(d,"class","svelte-m1khuf"),t(u,"class","svelte-m1khuf"),t(c,"class","svelte-m1khuf"),t(a,"class","book-desc svelte-m1khuf")},m(e,r){k(e,i,r),k(e,w,r),k(e,o,r),s(o,x),k(e,y,r),k(e,a,r),s(a,l),s(a,J),s(a,d),s(d,E),s(a,M),s(a,u),s(u,A),s(a,N),s(a,c),s(c,z)},p:B,i:B,o:B,d(e){e&&n(i),e&&n(w),e&&n(o),e&&n(y),e&&n(a)}}}class R extends H{constructor(i){super(),K(this,i,null,C,O,{})}}export{R as default};
